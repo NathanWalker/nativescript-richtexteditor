d49bb08ec187d27ce1012f05d6ffa8539e164ccf
Nathan Walker <walkerrunpdx@gmail.com>
2025-01-10 19:57:24 -0800

    feat: enable zoneless

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,7 @@ import { withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
-/**
- * Disable zone by setting this to true
- * Then also adjust polyfills.ts (see note there)
- */
-const EXPERIMENTAL_ZONELESS = false;
+const EXPERIMENTAL_ZONELESS = true;
 
 runNativeScriptAngularApp({
   appModuleBootstrap: () => {
